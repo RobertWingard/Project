555739041efa0edbc6f008400cc269a9fcf800b5
RobertWingard <RobertWingard@Icloud.com>
2022-09-21 13:28:15 -0700

    resize

--- server.py
+++ server.py
@@ -2,66 +2,6 @@ import pygame
 from pygame.locals import *
 import random
 
-
-
-
-
-
-
-#possibly add in dragonballs and go around and collect them to win the game
-#
-
-
-
-
-# import pygame module in this program 
-# def bouncing_rect():
-#         #draw rect
-#     global x_speed, y_speed, other_speed
-#     moving_rect.x += x_speed
-#     moving_rect.y += y_speed
-    #collision with screen borders
-    # if moving_rect.right >= screen_width or moving_rect.left <= 0:
-    #     x_speed *= -1
-    # if moving_rect.bottom >= screen_height or moving_rect.top <= 0:
-    #     y_speed *= -1
-
-
-
-# collision with screen borders
-    # if moving_rect.right >= screen_width and x_speed > 0:
-    #     x_speed *= -1
-    # if moving_rect.left <= 0 and x_speed < 0:
-    #     x_speed *= -1
-    # if moving_rect.top <= 0 and y_speed < 0:
-    #     y_speed *= -1
-    # if moving_rect.bottom >= screen_height and y_speed > 0:
-    #     y_speed *= -1
-
-
-
-        #moving other rect
-    # other_rect.y += other_speed
-    # if other_rect.top <= 0 or other_rect.bottom >= screen_height:
-    #     other_speed *= -1
-
-        #collision with rect
-    # collision_tolerance = 10
-    # if moving_rect.colliderect(other_rect):
-    #     if abs(other_rect.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:
-    #         y_speed *= -1
-    #     if abs(other_rect.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
-    #         y_speed *= -1
-    #     if abs(other_rect.right - moving_rect.left) < collision_tolerance and x_speed < 0:
-    #         x_speed *= -1
-    #     if abs(other_rect.left - moving_rect.right) < collision_tolerance and x_speed > 0:
-    #         x_speed *= -1
-
-
-    
-    # pygame.draw.rect(screen, (255,255,255),moving_rect)
-    # pygame.draw.rect(screen, (255,0,0),other_rect)
-
 pygame.init()
 screen_width = 1200
 screen_height = 800
@@ -98,28 +38,21 @@ height = 20
 vel = 10
 
 #hero
-goku = pygame.image.load("goku.png")
+goku = pygame.image.load("goku.xcf")
 goku_loc = goku.get_rect()
-goku = pygame.draw.rect(goku,(255,0,0),[0,0, width, height], 2 )
+print(type(goku_loc))
+
+# goku = pygame.draw.rect(goku,(255,0,0),[0,0, width, height], 2 )
 print(goku_loc)
 goku_loc.center = x,y
 
 
+
 #enemy
-mob = pygame.image.load("mob.png")
+mob = pygame.image.load("mob.xcf")
 mob_loc = mob.get_rect()
 print(mob_loc) 
-mob_loc.center = 100,200
-
-#test rect
-# moving_rect = pygame.Rect(350,350,100,100)
-# x_speed, y_speed = 5,4
-
-# #other test rect
-
-# other_rect = pygame.Rect(300,600,200,100)
-
-# other_speed = 2
+mob_loc.center = 10,10
 
 # Indicates pygame is running
 run = True
@@ -128,30 +61,11 @@ print(mob_loc.center)
 # infinite loop 
 while run:
 
-    (currentMobX, currentMobY) = mob_loc.center
-    currentMobXLower = currentMobX - 15, currentMobY
-    currentMobXUpper = currentMobX + 15, currentMobY
     #draw background
     draw_bg()
-<<<<<<< HEAD
 
-    #damage
-    # (currentMobX, currentMobY) = mob_loc.center
-    # currentMobXLower = currentMobX - 15, currentMobY
-    # currentMobXUpper = currentMobX + 15, currentMobY
-    # mob bottom left < goku x is < 
+
     
-
-
-
-    if goku_loc == mob_loc.center:
-=======
-    if goku_loc.center >= currentMobXLower or goku_loc.center <= currentMobXUpper:
-    # if mob_loc.center == goku_loc.center:
-    # if goku_loc[0] == mob_loc[0] and mob_loc[1] > goku_loc[1]:
->>>>>>> 9af9abf3d681b733b8f30b31a29a89311c1c90d2
-        print('game over')
-        break
 
     # creates time delay of 10ms 
     pygame.time.delay(10)
@@ -183,20 +97,21 @@ while run:
     if keys[pygame.K_DOWN] and y<screen_height-(height+goku.get_height()):
         # increment in y co-ordinate
         y += vel
-    # completely fill the surface object  
-    # with black colour  
-    # win.fill((0, 0, 0))
-    # drawing object on screen which is  a rectangle here 
-    # pygame.draw.rect(screen, (255, 255, 255), (x, y, width, height))
-    goku_loc = (x,y)
-    screen.blit(goku, goku_loc)
-    screen.blit(mob, (100,200))
 
 
-    # bouncing_rect()
+    screen.blit(goku, (x,y))
+    screen.blit(mob, (300,500))
 
-    # if goku_loc.colliderect(mob):
-    #     pygame.draw.rect(screen, (255,0,0), goku_loc, 4)
+    goku_loc = goku.get_rect(topleft=(x,y))
+    mob_loc = mob.get_rect(topleft=(300,500))
+
+
+    # print(goku_loc)
+    # print(mob_loc)
+    if goku_loc.colliderect(mob_loc): 
+        print('game over')
+        break
+
     # it refreshes the window
     pygame.display.update() 
 # closes the pygame window 
