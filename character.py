71ce28f132f8a594daeaafd8d85826a3a466d492
RobertWingard <RobertWingard@Icloud.com>
2022-09-22 07:58:50 -0700

    class update

--- character.py
+++ character.py
@@ -1,1 +1,27 @@
 import pygame
+
+
+
+class Character:
+    def init(self, start_pos, image, rect):
+        self.pos = start_pos
+        self.image = pygame.image.load(image)
+        self.update = self.rect = self.image.get_rect(topleft=self.pos)
+
+    def move_up(self):
+        self.pos[1] += 1
+
+    def move_down(self):
+        self.pos[1] += -1
+
+    def move_left(self):
+        self.pos[0] += 1
+
+    def move_right(self):
+        self.pos[0] += -1
+
+    def draw(self, screen):
+        screen.blit(self.image, self.pos)
+
+    def update(self):
+        self.rect = self.image.get_rect(topleft=self.pos)
